820e6cca9a1493e450ea5beb2bfd929d52a7e875
Elsa <elsasimonian902@gmail.com>
2025-09-26 20:38:03 +0400

    db 2.0

--- toDoList_backend/to_do_list.sql
+++ toDoList_backend/to_do_list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 16, 2025 at 01:39 PM
+-- Generation Time: Sep 26, 2025 at 06:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fulllearn1_to_do_list`
+-- Database: `to_do_list`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `categorie` varchar(20) NOT NULL
+  `category` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -41,7 +41,6 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `done_tasks` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
   `task_id` int(11) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -54,8 +53,7 @@ CREATE TABLE `done_tasks` (
 
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `category` varchar(20) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `task` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -68,7 +66,7 @@ CREATE TABLE `tasks` (
 CREATE TABLE `team_members` (
   `id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL,
-  `member_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -79,7 +77,6 @@ CREATE TABLE `team_members` (
 
 CREATE TABLE `team_tasks` (
   `id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL,
   `team_member_id` int(11) DEFAULT NULL,
   `task` varchar(20) NOT NULL,
   `deadline` date NOT NULL,
@@ -100,6 +97,13 @@ CREATE TABLE `team_task_name` (
   `deadline` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `team_task_name`
+--
+
+INSERT INTO `team_task_name` (`id`, `user_id`, `name`, `deadline`) VALUES
+(1, 4, 'wegwge', '2025-08-20');
+
 -- --------------------------------------------------------
 
 --
@@ -119,6 +123,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `theme`, `type`, `token`, `exp_date`, `verified`) VALUES
+(2, 'Maria', 'nazeni@gmail.com', '$2y$10$XK7WbB5sbakO1TQwmQ8bheYZ5mBODKL4im7DGwJnllvUhHCcz.Pda', '1', '2', '3d41f211314d29d2ad556d13d49c4722', '2025-09-22 08:40:11', '1'),
+(4, 'user_bhlvhwu', 'simonyanelsa@gmail.com', NULL, '1', '1', NULL, NULL, NULL);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -126,37 +138,44 @@ CREATE TABLE `users` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`category`);
 
 --
 -- Indexes for table `done_tasks`
 --
 ALTER TABLE `done_tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`);
 
 --
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id_` (`category_id`);
 
 --
 -- Indexes for table `team_members`
 --
 ALTER TABLE `team_members`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `project_id` (`project_id`,`user_id`) USING BTREE,
+  ADD KEY `user_id_key` (`user_id`);
 
 --
 -- Indexes for table `team_tasks`
 --
 ALTER TABLE `team_tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `team_tasks_member_id` (`team_member_id`);
 
 --
 -- Indexes for table `team_task_name`
 --
 ALTER TABLE `team_task_name`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `team_task_name_u_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -174,43 +193,84 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `done_tasks`
 --
 ALTER TABLE `done_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `team_members`
 --
 ALTER TABLE `team_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `team_tasks`
 --
 ALTER TABLE `team_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `team_task_name`
 --
 ALTER TABLE `team_task_name`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `done_tasks`
+--
+ALTER TABLE `done_tasks`
+  ADD CONSTRAINT `done_tasks_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `category_id_` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `team_members`
+--
+ALTER TABLE `team_members`
+  ADD CONSTRAINT `project_id_key` FOREIGN KEY (`project_id`) REFERENCES `team_task_name` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id_key` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `team_tasks`
+--
+ALTER TABLE `team_tasks`
+  ADD CONSTRAINT `team_tasks_member_id` FOREIGN KEY (`team_member_id`) REFERENCES `team_members` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `team_task_name`
+--
+ALTER TABLE `team_task_name`
+  ADD CONSTRAINT `team_task_name_u_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
